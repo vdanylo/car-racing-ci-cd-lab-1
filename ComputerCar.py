afbea47b8e78625ca81a5914001f5eff752441ee
vdanylo <danil.voronov@knu.ua>
2024-02-06 20:07:33 +0200

    danylo-the-seventh-commit-added-missing-image-file

--- ComputerCar.py
+++ ComputerCar.py
@@ -7,7 +7,7 @@ from Constants import GREEN_CAR
 
 
 class ComputerCar(AbstractCar):
-    # IMG = GREEN_CAR
+    IMG = GREEN_CAR
     START_POS = (150, 200)
 
     def __init__(self, max_vel, rotation_vel, path=[]):
