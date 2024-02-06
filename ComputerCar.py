3c9c03b3ea7b889ed70c5709d136c522f6f509bf
vdanylo <danil.voronov@knu.ua>
2024-02-06 19:10:49 +0200

    danylo-the-forth-commit-added-computer-car

---
+++ ComputerCar.py
@@ -0,0 +1,18 @@
+import math
+
+import pygame
+
+from AbstractCar import AbstractCar
+from Constants import GREEN_CAR
+
+
+class ComputerCar(AbstractCar):
+    IMG = GREEN_CAR
+    START_POS = (150, 200)
+
+    def __init__(self, max_vel, rotation_vel, path=[]):
+        super().__init__(max_vel, rotation_vel)
+        self.path = path
+        self.current_point = 0
+        self.vel = max_vel
+
