4686b08fa81f015fd0993c89f957382de914155b
vdanylo <danil.voronov@knu.ua>
2024-02-07 10:04:10 +0200

    danylo-the-eleventh-commit-implemented-player-car-logic

--- PlayerCar.py
+++ PlayerCar.py
@@ -6,3 +6,10 @@ class PlayerCar(AbstractCar):
     IMG = RED_CAR
     START_POS = (180, 200)
 
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
+
+    def bounce(self):
+        self.vel = -self.vel
+        self.move()
